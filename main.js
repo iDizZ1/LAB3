1b9730fb58d79b2ca57548a6bcea570c5d4445a6
iDizZ1 <idizz100@gmail.com>
2023-10-19 07:51:20 +0300

    2
    2

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ document.getElementById("btn").addEventListener("click", function(){
 document.getElementById("close-btn").addEventListener("click", function(){
     document.getElementById("my-modal").classList.remove("open")
 })
+
+document.querySelectorAll(".accord").forEach((el) => {
+    el.addEventListener("click", () => {
+        let cont = el.nextElementSibling;
+
+        if(cont.style.maxHeight) {
+            documeny.querySelectorAll(".cont").forEach((el) => el.style.maxHeight = null) 
+        } else {
+            document.querySelectorAll(".cont").forEach((el) => el.style.maxHeight = null)
+            cont.style.maxHeight = cont.scrollHeight + "px" 
+        }
+    })
+})
